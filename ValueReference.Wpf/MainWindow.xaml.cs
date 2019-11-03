cb34eae449b25781311da9afdc6644306c61565d
Dries Deboosere <dries.deboosere@howest.be>
2019-11-03 23:00:08 +0100

    Add project files.

---
+++ ValueReference.Wpf/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ValueReference.Wpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnCopyValueType_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void BtnCopyValueTypeEnum_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void BtnCopyValueTypeStruct_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void BtnCopyReferenceType_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void BtnCopyReferenceTypeString_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void BtnPassValueType_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void BtnPassValueTypeByRef_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void BtnOutputParameter_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void BtnBoxing_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void BtnUnboxing_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void BtnSafeCasting_Click(object sender, RoutedEventArgs e)
+        {
+        }
+    }
+}
